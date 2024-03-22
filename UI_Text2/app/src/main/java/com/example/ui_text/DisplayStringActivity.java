46ac803db1692470f1b5e34b2827b059b7a71616
mdyxuan <emily450033@gmail.com>
2024-03-22 14:09:55 +0800

    2024-03-22

---
+++ UI_Text2/app/src/main/java/com/example/ui_text/DisplayStringActivity.java
@@ -0,0 +1,22 @@
+package com.example.ui_text;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class DisplayStringActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.layout_display_string);
+
+        TextView txvShow =  (TextView) findViewById(R.id.txvShow);
+        Intent intent = getIntent();
+        String receivedString = intent.getStringExtra("UserInput");
+
+        txvShow.setText("輸入: "+ receivedString);
+    }
+}
