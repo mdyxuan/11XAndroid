ccb0498cbdb1ec14b47a8852f9efaf713b9ca52c
mdyxuan <emily450033@gmail.com>
2024-04-26 12:57:43 +0800

    2024-04-26

--- Ch5_5/app/src/main/java/com/example/ch5_5/MainActivity.java
+++ Ch5_5/app/src/main/java/com/example/ch5_5/MainActivity.java
@@ -3,8 +3,11 @@ package com.example.ch5_5;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.constraintlayout.widget.ConstraintLayout;
 
+import android.content.Context;
 import android.graphics.Color;
 import android.os.Bundle;
+import android.os.VibrationEffect;
+import android.os.Vibrator;
 import android.view.MotionEvent;
 import android.view.View;
 import android.widget.TextView;
@@ -26,11 +29,13 @@ public class MainActivity extends AppCompatActivity implements View.OnTouchListe
     public boolean onTouch(View v, MotionEvent event)
     {
         int act = event.getAction();
+        Vibrator vb = (Vibrator) getSystemService(Context.VIBRATOR_SERVICE);
         switch (act)
         {
             case MotionEvent.ACTION_DOWN:
                 txvOutput.setText("ACTION_DOWN");
                 txvOutput.setTextColor(Color.RED);
+                vb.vibrate(VibrationEffect.createOneShot(2000,20));
                 break;
             case MotionEvent.ACTION_UP:
                 txvOutput.setText("ACTION_UP");
